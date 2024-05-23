d9c1c952fbc15e8d05d25df8bd1ea1503ee39000
Usmonov Doston <usmonov.d@ad.asialuxe.uz>
2024-05-23 17:35:30 +0500

    login page and start side bar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,6 @@
 <template>
-  <div>
-    <div class="app-head">
-      <RouterLink to="/" exact-active-class="link-active">Home</RouterLink>
-      <RouterLink to="/about" exact-active-class="link-active"
-        >About</RouterLink
-      >
-    </div>
-    <router-view />
-    <footer>
-      <a href="https://github.com/manuchekhr32/vue-starter"
-        >Hit a star GitHub repo</a
-      >
-    </footer>
-  </div>
+  <CSideBar/>
 </template>
+<script setup lang="ts">
+import CSideBar from "@/components/CSideBar.vue";
+</script>
---
+++ src/components/CSideBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
+    <el-radio-button :value="false">expand</el-radio-button>
+    <el-radio-button :value="true">collapse</el-radio-button>
+  </el-radio-group>
+  <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      :collapse="isCollapse"
+  >
+    <el-menu-item index="2">
+      <X />
+      <template #title>Navigator Two</template>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <template #title>Navigator Four</template>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { X } from 'lucide-vue-next';
+
+const isCollapse = ref(true)
+
+</script>
+
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 100vh;
+}
+
+.el-menu {
+  min-height: 100vh;
+}
+</style>
