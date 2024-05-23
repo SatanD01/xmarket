cf26f421dfd23c500d30b31df85772782263fde4
Usmonov Doston <usmonov.d@ad.asialuxe.uz>
2024-05-23 18:27:42 +0500

    menu

--- src/components/CSideBar.vue
+++ src/components/CSideBar.vue
@@ -1,34 +1,98 @@
 <template>
-  <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
-    <el-radio-button :value="false">expand</el-radio-button>
-    <el-radio-button :value="true">collapse</el-radio-button>
-  </el-radio-group>
   <el-menu
       default-active="2"
       class="el-menu-vertical-demo"
       :collapse="isCollapse"
   >
+
+    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
+      <el-radio-button :value="false">expand</el-radio-button>
+      <el-radio-button :value="true">collapse</el-radio-button>
+    </el-radio-group>
+    <a v-if="!isCollapse" href="/" class="block px-5 py-4">
+      <img src="/logo_black.png" alt="logo">
+    </a>
+    <a v-else href="/" class="block px-5 py-4">
+      <X class="bg-black text-white rounded" />
+    </a>
+    <el-menu-item index="1">
+      <Box />
+      <template #title>
+        <span class="ms-2 font-semibold">
+          Товары
+        </span>
+      </template>
+    </el-menu-item>
     <el-menu-item index="2">
-      <X />
-      <template #title>Navigator Two</template>
+      <AreaChart />
+      <template #title>
+        <span class="ms-2 font-semibold">
+          График
+        </span>
+      </template>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <ShoppingCart />
+      <template #title>
+        <span class="ms-2 font-semibold">
+          Создать заказ
+        </span>
+      </template>
     </el-menu-item>
     <el-menu-item index="4">
-      <template #title>Navigator Four</template>
+      <QrCode />
+      <template #title>
+        <span class="ms-2 font-semibold">
+          Сканировать QR-Код
+        </span>
+      </template>
+    </el-menu-item>
+    <el-menu-item index="5">
+      <PackageOpen />
+      <template #title>
+        <span class="ms-2 font-semibold">
+          Создать товар
+        </span>
+      </template>
+    </el-menu-item>
+    <el-menu-item index="6">
+      <Home />
+      <template #title>
+        <span class="ms-2 font-semibold">
+          Создать офис
+        </span>
+      </template>
+    </el-menu-item>
+    <el-menu-item index="7">
+      <UserRoundPlus />
+      <template #title>
+        <span class="ms-2 font-semibold">
+          Создать пользователя
+        </span>
+      </template>
+    </el-menu-item>
+    <el-menu-item index="8">
+      <Settings />
+      <template #title>
+        <span class="ms-2 font-semibold">
+          Админ настройки
+        </span>
+      </template>
     </el-menu-item>
   </el-menu>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { X } from 'lucide-vue-next';
+import { QrCode, X, Box, ShoppingCart, AreaChart, Settings, Home, PackageOpen, UserRoundPlus } from 'lucide-vue-next';
 
-const isCollapse = ref(true)
+const isCollapse = ref(false)
 
 </script>
 
 <style>
 .el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
+  width: 230px;
   min-height: 100vh;
 }
 
