894449bd6ca3a1550de1fd83b1cad2f5605d5294
Usmonov Doston <usmonov.d@ad.asialuxe.uz>
2024-05-24 18:55:26 +0500

    dist

---
+++ dist/assets/LDefault-cwHi8Kqi.js
@@ -0,0 +1,51 @@
+import{c as n}from"./createLucideIcon-t2yiBBxo.js";import{d as x,r as g,a as s,o as i,c as b,b as o,n as I,e,w as t,u as a,f as h,p as V,g as z,_ as $,h as v}from"./index-AaAgFkZj.js";/**
+ * @license lucide-vue-next v0.379.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const L=n("AreaChartIcon",[["path",{d:"M3 3v18h18",key:"1s2lah"}],["path",{d:"M7 12v5h12V8l-5 5-4-4Z",key:"zxz28u"}]]);/**
+ * @license lucide-vue-next v0.379.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const B=n("BoxIcon",[["path",{d:"M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z",key:"hh9hay"}],["path",{d:"m3.3 7 8.7 5 8.7-5",key:"g66t2b"}],["path",{d:"M12 22V12",key:"d0xqtd"}]]);/**
+ * @license lucide-vue-next v0.379.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const H=n("HomeIcon",[["path",{d:"m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"y5dka4"}],["polyline",{points:"9 22 9 12 15 12 15 22",key:"e2us08"}]]);/**
+ * @license lucide-vue-next v0.379.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const S=n("LogOutIcon",[["path",{d:"M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4",key:"1uf3rs"}],["polyline",{points:"16 17 21 12 16 7",key:"1gabdz"}],["line",{x1:"21",x2:"9",y1:"12",y2:"12",key:"1uyos4"}]]);/**
+ * @license lucide-vue-next v0.379.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const q=n("PackageOpenIcon",[["path",{d:"M12 22v-9",key:"x3hkom"}],["path",{d:"M15.17 2.21a1.67 1.67 0 0 1 1.63 0L21 4.57a1.93 1.93 0 0 1 0 3.36L8.82 14.79a1.655 1.655 0 0 1-1.64 0L3 12.43a1.93 1.93 0 0 1 0-3.36z",key:"2ntwy6"}],["path",{d:"M20 13v3.87a2.06 2.06 0 0 1-1.11 1.83l-6 3.08a1.93 1.93 0 0 1-1.78 0l-6-3.08A2.06 2.06 0 0 1 4 16.87V13",key:"1pmm1c"}],["path",{d:"M21 12.43a1.93 1.93 0 0 0 0-3.36L8.83 2.2a1.64 1.64 0 0 0-1.63 0L3 4.57a1.93 1.93 0 0 0 0 3.36l12.18 6.86a1.636 1.636 0 0 0 1.63 0z",key:"12ttoo"}]]);/**
+ * @license lucide-vue-next v0.379.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const j=n("QrCodeIcon",[["rect",{width:"5",height:"5",x:"3",y:"3",rx:"1",key:"1tu5fj"}],["rect",{width:"5",height:"5",x:"16",y:"3",rx:"1",key:"1v8r4q"}],["rect",{width:"5",height:"5",x:"3",y:"16",rx:"1",key:"1x03jg"}],["path",{d:"M21 16h-3a2 2 0 0 0-2 2v3",key:"177gqh"}],["path",{d:"M21 21v.01",key:"ents32"}],["path",{d:"M12 7v3a2 2 0 0 1-2 2H7",key:"8crl2c"}],["path",{d:"M3 12h.01",key:"nlz23k"}],["path",{d:"M12 3h.01",key:"n36tog"}],["path",{d:"M12 16v.01",key:"133mhm"}],["path",{d:"M16 12h1",key:"1slzba"}],["path",{d:"M21 12v.01",key:"1lwtk9"}],["path",{d:"M12 21v-1",key:"1880an"}]]);/**
+ * @license lucide-vue-next v0.379.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const M=n("SettingsIcon",[["path",{d:"M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z",key:"1qme2f"}],["circle",{cx:"12",cy:"12",r:"3",key:"1v7zrd"}]]);/**
+ * @license lucide-vue-next v0.379.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const w=n("ShoppingCartIcon",[["circle",{cx:"8",cy:"21",r:"1",key:"jimo8o"}],["circle",{cx:"19",cy:"21",r:"1",key:"13723u"}],["path",{d:"M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12",key:"9zh506"}]]);/**
+ * @license lucide-vue-next v0.379.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const A=n("UserRoundPlusIcon",[["path",{d:"M2 21a8 8 0 0 1 13.292-6",key:"bjp14o"}],["circle",{cx:"10",cy:"8",r:"5",key:"o932ke"}],["path",{d:"M19 16v6",key:"tddt3s"}],["path",{d:"M22 19h-6",key:"vcuq98"}]]);/**
+ * @license lucide-vue-next v0.379.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const R=n("XIcon",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]]),_=d=>(V("data-v-884b6fb1"),d=d(),z(),d),O={class:"bg-white w-full flex justify-between items-center border-b p-1 sticky top-0 z-[100]"},P=_(()=>o("div",{class:"line line1"},null,-1)),N=_(()=>o("div",{class:"line line2"},null,-1)),Q=_(()=>o("div",{class:"line line3"},null,-1)),U=[P,N,Q],X={class:"flex items-center gap-5 cursor-pointer"},Z={class:"border group w-[40px] h-[40px] rounded flex items-center justify-center hover:transition-all transition-all hover:border-[#409eef] hover:text-[409eef]"},D=_(()=>o("span",{class:"ms-2"},"Настройки профиля",-1)),E=_(()=>o("span",{class:"ms-2"},"Выйти",-1)),T=x({__name:"CHeader",setup(d){const c=g(!1),m=()=>{c.value=!c.value};return(r,f)=>{const y=s("el-badge"),p=s("router-link"),l=s("el-avatar"),u=s("el-dropdown-item"),k=s("el-dropdown-menu"),C=s("el-dropdown");return i(),b("header",O,[o("div",{class:"burger-container",onClick:m},[o("div",{class:I(["burger",{"burger-active":c.value}])},U,2)]),o("div",X,[e(p,{to:"/cart"},{default:t(()=>[e(y,{value:12,type:"primary"},{default:t(()=>[o("div",Z,[e(a(w),{class:"group-hover:text-[#409eef] group-hover:transition-all transition-all"})])]),_:1})]),_:1}),e(C,null,{dropdown:t(()=>[e(k,null,{default:t(()=>[e(u,null,{default:t(()=>[e(a(M)),h(),D]),_:1}),e(u,null,{default:t(()=>[e(a(S)),h(),E]),_:1})]),_:1})]),default:t(()=>[e(l,{shape:"square",class:"mb-0",src:"https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png"})]),_:1})])])}}}),F=$(T,[["__scopeId","data-v-884b6fb1"]]),G="https://satand01.github.io/xmarket/logo_black.png",J=o("img",{src:G,alt:"logo"},null,-1),K=o("span",{class:"ms-2 font-semibold"}," ТОП Товары ",-1),W=o("span",{class:"ms-2 font-semibold"}," График ",-1),Y=o("span",{class:"ms-2 font-semibold"}," Создать заказ ",-1),ee=o("span",{class:"ms-2 font-semibold"}," Сканировать QR-Код ",-1),te=o("span",{class:"ms-2 font-semibold"}," Создать товар ",-1),oe=o("span",{class:"ms-2 font-semibold"}," Создать офис ",-1),ae=o("span",{class:"ms-2 font-semibold"}," Создать пользователя ",-1),se=o("span",{class:"ms-2 font-semibold"}," Админ настройки ",-1),le=x({__name:"CSideBar",setup(d){const c=g(!1);return(m,r)=>{const f=s("el-radio-button"),y=s("el-radio-group"),p=s("router-link"),l=s("el-menu-item"),u=s("el-menu");return i(),v(u,{"default-active":"2",class:"el-menu-vertical-demo",collapse:c.value,router:""},{default:t(()=>[e(y,{modelValue:c.value,"onUpdate:modelValue":r[0]||(r[0]=k=>c.value=k),style:{"margin-bottom":"20px"}},{default:t(()=>[e(f,{value:!1},{default:t(()=>[h("expand")]),_:1}),e(f,{value:!0},{default:t(()=>[h("collapse")]),_:1})]),_:1},8,["modelValue"]),c.value?(i(),v(p,{key:1,to:"/",class:"block px-5 py-4"},{default:t(()=>[e(a(R),{class:"bg-black text-white rounded"})]),_:1})):(i(),v(p,{key:0,to:"/",class:"block px-5 py-4"},{default:t(()=>[J]),_:1})),e(l,{index:"1",route:"/"},{title:t(()=>[K]),default:t(()=>[e(a(B))]),_:1}),e(l,{index:"2",route:"/chart"},{title:t(()=>[W]),default:t(()=>[e(a(L))]),_:1}),e(l,{index:"3",route:"/create-order"},{title:t(()=>[Y]),default:t(()=>[e(a(w))]),_:1}),e(l,{index:"4",route:"/qr-scan"},{title:t(()=>[ee]),default:t(()=>[e(a(j))]),_:1}),e(l,{index:"5",route:"/create-goods"},{title:t(()=>[te]),default:t(()=>[e(a(q))]),_:1}),e(l,{index:"6",route:"/create-office"},{title:t(()=>[oe]),default:t(()=>[e(a(H))]),_:1}),e(l,{index:"7",route:"/create-user"},{title:t(()=>[ae]),default:t(()=>[e(a(A))]),_:1}),e(l,{index:"8",route:"/admin-settings"},{title:t(()=>[se]),default:t(()=>[e(a(M))]),_:1})]),_:1},8,["collapse"])}}}),ne={class:"grid grid-cols-[230px_1fr]"},ce={class:"p-5"},ie=x({__name:"LDefault",setup(d){return(c,m)=>{const r=s("RouterView");return i(),b("div",ne,[e(le),o("div",null,[e(F),o("div",ce,[e(r)])])])}}});export{ie as default};
