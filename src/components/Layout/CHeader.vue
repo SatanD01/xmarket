291ba2cd01e0dc1229e2e367766b96517922f1a2
Usmonov Doston <usmonov.d@ad.asialuxe.uz>
2024-05-24 10:51:30 +0500

    header

--- src/components/Layout/CHeader.vue
+++ src/components/Layout/CHeader.vue
@@ -1,8 +1,66 @@
 <template>
-  <header class="p-3 shadow-md border-b">
-    <div>Header component comes here</div>
-    <div></div>
+  <header class="bg-white w-full flex justify-between items-center border-b p-3 sticky top-0">
+    <div class="burger-container" @click="toggleBurger">
+      <div :class="['burger', { 'burger-active': isActive }]">
+        <div class="line line1"></div>
+        <div class="line line2"></div>
+        <div class="line line3"></div>
+      </div>
+    </div>
+    <el-dropdown>
+      <el-avatar shape="square" class="mb-0" src="https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png" />
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item> <Settings /> <span class="ms-2">Настройки профиля</span></el-dropdown-item>
+          <el-dropdown-item> <LogOut /> <span class="ms-2">Выйти</span></el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
   </header>
 </template>
-<script setup lang="ts"></script>
-<style scoped></style>
+<script setup lang="ts">
+import { Settings, LogOut } from 'lucide-vue-next';
+import { ref } from 'vue';
+
+const isActive = ref(false);
+
+const toggleBurger = () => {
+  isActive.value = !isActive.value;
+};
+</script>
+<style scoped>
+.burger-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  width: 50px;
+  height: 50px;
+}
+
+.burger {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 30px;
+  height: 20px;
+  transition: transform 0.3s ease;
+}
+
+.line {
+  background-color: black;
+  height: 4px;
+  border-radius: 2px;
+  transition: transform 0.3s ease, opacity 0.3s ease;
+}
+
+.burger-active .line1 {
+  transform: rotate(-45deg) translate(-4px, 2px);
+  width: 14px;
+}
+
+.burger-active .line3 {
+  transform: rotate(49deg) translate(-4px, -1.3px);
+  width: 14px;
+}
+</style>
