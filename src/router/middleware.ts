713329519010a0c50a963542dfcee6ecc3da73eb
Doston <gameplayer140701@gmail.com>
2024-07-19 15:55:54 +0300

    middlewere

--- src/router/middleware.ts
+++ src/router/middleware.ts
@@ -1,22 +1,31 @@
 import { computed } from 'vue'
 
-// import { useLayoutStore } from '@/layouts/store.ts'
 import { useAuthStore } from '@/modules/Auth/store'
 
 export async function checkRouteIsExist(to) {
   const store = useAuthStore()
   const token = store.getTokens()
   const user = computed(() => store.user)
+
   if (['404', '403', '500'].includes((to.name || '').toString())) {
     return true
   }
+
   if (!token && to.name !== 'Login') return { name: 'Login' }
+
   if (token && !user.value) {
     await store.fetchUser()
   }
 
-  if (!to.meta?.roles?.includes(user.value?.role)) return { name: 'Dashboard' }
+  if (
+    user.value &&
+    to.meta?.roles &&
+    !to.meta.roles.includes(user.value.role)
+  ) {
+    return { name: 'Dashboard' }
+  }
 
   if (user.value && to.name === 'Login') return { name: 'Dashboard' }
+
   return true
 }
